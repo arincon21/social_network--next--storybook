3d890d68a9067e17afa7d6b5664c352e5eee6f56
Alvaro Rincon <arincon@outlook.com>
2025-09-08 20:06:54 -0500

    Add: navbar top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
+import Navbar from "@/components/layouts/Navbar";
 import SidebarLeft from "@/components/layouts/sidebar-left";
-import Button from "@/components/ui/button";
-import Card from "@/components/ui/card";
-import Icon from "@/components/ui/icon";
 
 export default function Home() {
   return (
     <div className="p-10 flex flex-col gap-5">
-      <Card>
-        <p>This is the card content.</p>
-      </Card>
-      <Button variant="secondary" size="md">Click Me</Button>
-      <Button variant="primary" size="md">Click Me</Button>
-      <Icon name="olymp-logout-icon" className="w-5 h-5 text-gray-600" />
-
+      <Navbar />
       <SidebarLeft/>
     </div>
   );
